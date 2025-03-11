e8b28c429452f6e29acba4f636c3b342c4c3f8a4
JesperJigs <jesperjigelius@gmail.com>
2025-03-11 09:24:10 +0100

    fixed order

--- src/main/java/Builder/SkirtBuilder.java
+++ src/main/java/Builder/SkirtBuilder.java
@@ -11,14 +11,6 @@ public class SkirtBuilder {
         return this;
     }
 
-    public SkirtBuilder setColor(String color) {
-        if (color == null || color.isEmpty()) {
-            throw new IllegalArgumentException("Color is missing");
-        }
-        skirt.setColor(color);
-        return this;
-    }
-
     public SkirtBuilder setSize(String size) {
         if (size == null || size.isEmpty()) {
             throw new IllegalArgumentException("Size is missing");
@@ -26,7 +18,24 @@ public class SkirtBuilder {
         skirt.setSize(size);
         return this;
     }
+
+    public SkirtBuilder setColor(String color) {
+        if (color == null || color.isEmpty()) {
+            throw new IllegalArgumentException("Color is missing");
+        }
+        skirt.setColor(color);
+        return this;
+    }
     public Skirt build() {
+        if (skirt.getMaterial() == null || skirt.getMaterial().isEmpty()) {
+            throw new IllegalStateException("Skirt must have a material");
+        }
+        if (skirt.getSize() == null || skirt.getSize().isEmpty()) {
+            throw new IllegalStateException("Skirt must have a size");
+        }
+        if (skirt.getColor() == null || skirt.getColor().isEmpty()) {
+            throw new IllegalStateException("Skirt must have a color");
+        }
         return skirt;
     }
 }
--- src/main/java/Builder/TshirtBuilder.java
+++ src/main/java/Builder/TshirtBuilder.java
@@ -10,14 +10,6 @@ public class TshirtBuilder {
         tshirt.setMaterial(material);
         return this;
     }
-    public TshirtBuilder setColor(String color) {
-        if (color == null || color.isEmpty()) {
-            tshirt.setColor(color);
-        }
-        tshirt.setColor(color);
-        return this;
-    }
-
     public TshirtBuilder setSize(String size) {
         if (size == null || size.isEmpty()) {
             tshirt.setSize(size);
@@ -25,7 +17,24 @@ public class TshirtBuilder {
         tshirt.setSize(size);
         return this;
     }
+
+    public TshirtBuilder setColor(String color) {
+        if (color == null || color.isEmpty()) {
+            tshirt.setColor(color);
+        }
+        tshirt.setColor(color);
+        return this;
+    }
     public Tshirt build() {
+        if (tshirt.getMaterial() == null || tshirt.getMaterial().isEmpty()) {
+            throw new IllegalStateException("Tshirt must have a material");
+        }
+        if (tshirt.getSize() == null || tshirt.getSize().isEmpty()) {
+            throw new IllegalStateException("Tshirt must have a size");
+        }
+        if (tshirt.getColor() == null || tshirt.getColor().isEmpty()) {
+            throw new IllegalStateException("Tshirt must have a color");
+        }
         return tshirt;
 
     }
